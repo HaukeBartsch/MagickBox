c3dff5d1f1afc25e97a99c271abc638ac91a42d4
Hauke Bartsch <haukebartsch@gmail.com>
2013-11-22 08:45:50 -0800

    change to python to be able to read json

--- code/bin/routing.sh
+++ code/bin/routing.sh
@@ -1,3 +1,38 @@
+#!/usr/bin/env python
+
+import sys, json
+from pprint import pprint
+
+def main(argv):
+  if len(sys.argv) != 4:
+     print "usage: <processing directory> <aetitle called> <aetitle caller>"
+     sys.exit()
+  WORKINGDIR=sys.argv[1]
+  AETitleCalled=sys.argv[2]
+  AETitleCaller=sys.argv[3]
+
+  routingtable_data = open('/data/code/bin/routing.json')
+  routingtable = json.load(routingtable_data)
+  routingtable_data.close()
+  pprint(routingtable)
+
+  #try:
+  #  idx = header.index(c)
+  #except ValueError:
+  #  print "Error: column does not exist"
+  #  sys.exit(-1)
+
+  #all = list( reader )
+  #try:
+  #  print all[s][idx]
+  #except IndexError:
+  #  print "Error: step does not exist"
+  #  sys.exit(-1)
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
+
+
 #!/bin/bash
 . ticktick.sh
 
@@ -18,7 +53,7 @@ DATA=`cat /data/code/bin/routing.json`
 tickParse "$DATA"
 
 echo "read routing information: $DATA" >> /data/logs/routing.log
-echo ``routing[0][AETitleIn]`` >> /data/logs/routing.log
+echo "one route: ``routing[0][AETitleIn]`` " >> /data/logs/routing.log
 
 numRoutes=``routing.length()``
 echo "number of routes $numRoutes" >> /data/logs/routing.log
