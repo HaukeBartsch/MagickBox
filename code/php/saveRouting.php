ea1210c522e89189661cab4f844524380ba8908c
Hauke Bartsch <haukebartsch@gmail.com>
2013-11-22 16:14:33 -0800

    save file again to disk

--- code/php/saveRouting.php
+++ code/php/saveRouting.php
@@ -3,7 +3,7 @@
   if ( isset($_GET['text']) ) {
   	$objs = json_decode($_GET['text'], true);
   	if ($objs == null) {
-  		echo "Error: could not parse this json file"
+  		echo "Error: could not parse this json file";
   	} else {
   	    file_put_contents('/data/code/bin/routing.json', json_encode($objs));
   	}
