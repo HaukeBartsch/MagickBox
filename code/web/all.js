adb81c86a07d934a01af53135203266d9472a599
Hauke Bartsch <hbartsch@ucsd.edu>
2014-07-28 17:30:27 -0700

    create date from year month and day

--- code/web/all.js
+++ code/web/all.js
@@ -182,7 +182,11 @@ function timeOverview( data ) {
    }*/
    var caldata = {};
    for (var i = 0; i < data.length; i++) {
-      timestamp = new date(data[i]['received']).getTime();
+      var year = data[i]['received'].split(' ').splice(-1)[0];
+      var day = data[i]['received'].split(' ').splice(2)[0];
+      var month = data[i]['received'].split(' ').splice(1)[0];
+      var m = { Jan: 0, Feb: 1, Mar: 2, Apr: 3, May: 4, Jun: 5, Jul: 6, Aug: 7, Sep: 8, Oct: 9, Nov: 10, Dec: 11 };
+      timestamp = new Date(year, m[month], day).getTime();
       caldata[timestamp] = 1;
    }
 
