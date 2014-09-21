ae7ac720631b6282b2991eb66f4420e35db63266
Hauke Bartsch <hbartsch@ucsd.edu>
2014-09-20 19:41:28 -0700

    create proper path name for downloaded OUTPUT

--- code/php/getOutputZip.php
+++ code/php/getOutputZip.php
@@ -6,8 +6,9 @@
   if ($DATA === "")
     return;
 
-  $fn = '/tmp/'.$DATA.'.zip';
-  exec("cd /data/scratch/$DATA/OUTPUT; zip -r ".$fn." *");
+  $fn = '/tmp/'.$DATA;
+  //exec("cd /data/scratch/$DATA/OUTPUT; zip -r ".$fn." *");
+  exec("cd /data/scratch/; zip -r ".$fn." $DATA -i \"*/OUTPUT/*\"");
   header("Content-Disposition: attachment; filename=".$DATA.".zip");
   header("Content-Type: application/zip");
   readfile( $fn );
