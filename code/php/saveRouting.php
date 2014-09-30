928367d5cb1ba028ff6d98d10470bcf19ac6d5c9
Hauke Bartsch <hbartsch@ucsd.edu>
2014-09-30 11:07:28 -0700

    use pretty print for saving routing information

--- code/php/saveRouting.php
+++ code/php/saveRouting.php
@@ -3,9 +3,9 @@
   if ( isset($_POST['text']) ) {
   	$objs = json_decode($_POST['text'], true);
   	if ($objs == null) {
-  		echo "Error: could not parse this json file";
+  	  echo "Error: could not parse this json file";
   	} else {
-  	    file_put_contents('/data/code/bin/routing.json', $_POST['text']);
+  	  file_put_contents('/data/code/bin/routing.json', json_encode($objs, JSON_PRETTY_PRINT));
   	}
   }
 ?>
