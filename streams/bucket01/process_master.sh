e59c4f05ea7aefb0b591b8a17f62ae28428625f6
Hauke Bartsch <hbartsch@ucsd.edu>
2015-02-28 15:09:41 -0800

    only parse output

--- streams/bucket01/process_master.sh
+++ streams/bucket01/process_master.sh
@@ -89,5 +89,5 @@ echo "`date`: Process bucket01 (routing is being performed)..." >> /data/logs/bu
 # implement data extraction
 echo "`date`: Start data extraction..." >> /data/logs/bucket01.log
 aet=`echo $AETitleCaller | sed -e 's/"//g'`
-/usr/bin/curl -G -d "sender=${aet}&parse=${WORKINGDIR}" ${PARENTIP}:${WEBPORT}/code/php/db.php
+/usr/bin/curl -G -d "sender=${aet}&parse=${WORKINGDIR}/OUTPUT" ${PARENTIP}:${WEBPORT}/code/php/db.php
 echo "`date`: End data extraction..." >> /data/logs/bucket01.log
