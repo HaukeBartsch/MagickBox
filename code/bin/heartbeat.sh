6efadb3c0c29ee97179f3d15c1c0cb84d76ed15b
Hauke Bartsch <HaukeBartsch@gmail.com>
2015-05-07 10:24:19 -0700

    we sometimes have to kill every job that listens to our port - in a loop

--- code/bin/heartbeat.sh
+++ code/bin/heartbeat.sh
@@ -16,19 +16,32 @@
 
 log=/data/logs/heartbeat.log
 
-#echo "try now: /usr/bin/echoscu $PARENTIP $PARENTPORT"
+echo "`date` - try now: /usr/bin/echoscu $PARENTIP $PARENTPORT" >> $log
 timeout 10 /usr/bin/echoscu $PARENTIP $PARENTPORT
 if (($? == 124)); then
    # get pid of storescu
-   pid=`pgrep storesp`
-   if [ "$pid" == "" ]; then
-      echo "storescp could not be found" >> $log
+   pid=`pgrep "storescp"`
+   if [ -z "$pid" ]; then
+      echo "storescp's pid could not be found" >> $log
       exit 0
    fi
-   echo "`date`: detected unresponsive storescp, kill and hope that monit restarts it" >> $log
+   echo "`date`: detected unresponsive storescp, kill \"$pid\" and hope that monit restarts it" >> $log
    # stop storescu gracefully first
    kill -s SIGTERM $pid && kill -0 $pid || exit 0
    sleep 5
    # more forceful
    kill -s SIGKILL $pid
+
+   # if we had to kill the process this way they port will belong to a parent, lets kill all of those as well
+   portstr=`netstat -lnp | grep $PARENTPORT`
+   while [ ! -z "$portstr" ]; do
+      echo "the port is still in use..." >> $log
+      
+      proc=`netstat -lnp | grep $PARENTPORT | cut -d'/' -f2`
+      id=`netstat -lnp | grep $PARENTPORT | cut -d'/' -f 1 | awk '{ print $7 }'`
+      echo "the port is still in use by a process ($proc) with id $id, kill it" >> $log
+      kill $id
+      # and check again
+      portstr=`netstat -lnp | grep $PARENTPORT`   
+   done
 fi
