1495efceb3d97ccbe296eeb80704282386cbbda6
Hauke Bartsch <HaukeBartsch@gmail.com>
2021-09-06 17:59:46 +0200

    convert to python3

--- code/bin/processSingleFile.py
+++ code/bin/processSingleFile.py
@@ -1,4 +1,4 @@
-#!/Usr/bin/env python
+#!/Usr/bin/env python3
 """
 Create a daemon process that listens to send messages and reads a DICOM file,
 extracts the header information and creates a Study/Series symbolic link structure.
@@ -34,7 +34,7 @@ class Daemon:
                                 if pid > 0:
                                             # exit first parent
                                             sys.exit(0)
-                    except OSError, e:
+                    except OSError as e:
                                 sys.stderr.write("fork #1 failed: %d (%s)\n" % (e.errno, e.strerror))
                                 sys.exit(1)
 
@@ -49,7 +49,7 @@ class Daemon:
                                 if pid > 0:
                                             # exit from second parent
                                             sys.exit(0)
-                    except OSError, e:
+                    except OSError as e:
                                 sys.stderr.write("fork #2 failed: %d (%s)\n" % (e.errno, e.strerror))
                                 sys.exit(1)
 
@@ -149,7 +149,7 @@ class Daemon:
                                 while 1:
                                             os.kill(pid, SIGTERM)
                                             time.sleep(0.1)
-                    except OSError, err:
+                    except OSError as err:
                                 err = str(err)
                                 if err.find("No such process") > 0:
                                             if os.path.exists(self.pidfile):
