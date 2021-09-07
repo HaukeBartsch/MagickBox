286aef562e11480bdde8e3afddd42f284a348157
Hauke Bartsch <HaukeBartsch@gmail.com>
2021-09-07 16:30:42 +0200

    fix: string replace

--- code/bucketstore/index.php
+++ code/bucketstore/index.php
@@ -43,7 +43,7 @@
    $dirs = array_filter(glob('/data/code/bucketstore/buckets/*'), 'is_dir');
    echo "<script type='text/javascript'>var numBuckets = ".count($dirs).";</script>";
    foreach ($dirs as $d) {
-      $d2 = replace($d, '/data', '');
+      $d2 = str_replace("/data", "", $d);
       $vals = json_decode( file_get_contents( $d2."/info.json"), true);
       echo "<li class='bucket'><div class='text'>".$vals['name']." - (v". $vals['version'] . ") <br/><a href='".$d."/".$vals['install']."'>DOWNLOAD</a></div></li>";
    }
